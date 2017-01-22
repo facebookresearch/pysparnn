ed3973b3aa21fe9ce2b5d4199bcc8fd5aa3d6a6c
Spencer Beecher <spencebeecher@fb.com>
2017-01-22 15:07:11 -0800

    updates

--- examples/pysparnn_utils.py
+++ examples/pysparnn_utils.py
@@ -1,9 +1,8 @@
-# Copyright (c) 2016-present, Facebook, Inc.
+# Copyright 2016-present, Facebook, Inc.
 # All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree. An additional grant
-# of patent rights can be found in the PATENTS file in the same directory
+
+# This source code is licensed under the license found in the
+# LICENSE-examples file in the root directory of this source tree.
 import numpy as np
 
 # code that will measure query time and recall
